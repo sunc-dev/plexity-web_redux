48eaddec8eef8c85ef7d5622fe8824a47c4dea4b
acurious-designer <50954166+sunc-dev@users.noreply.github.com>
2019-07-25 15:46:19 -0400

    initial

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -12,4 +12,29 @@ ScrollOut({
         // re-add the animated cl
         keylines.classList.add("animation-go");
     }
+
+});
+
+
+var os = new OnScreen({
+    tolerance: 0,
+    debounce: 100,
+    container: window
+});
+
+$('.about-mission-matte').onScreen({
+    container: window,
+    direction: 'vertical',
+    doIn: function () {
+        $('.about-mission-matte').classList.add("animation-go");
+    },
+    doOut: function () {
+        $('.about-mission-matte').classList.remove("animation-go");
+    },
+    tolerance: 0,
+    throttle: 50,
+    toggleClass: 'onScreen',
+    lazyAttr: null,
+    lazyPlaceholder: 'someImage.jpg',
+    debug: false
 });
